b43536d4ffeda657ef820b54c4ccd56bf2b08164
tjtanaa <tunjian.tan@embeddedllm.com>
2025-05-29 10:10:29 +0000

    add simplified initaiter
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

---
+++ initaiter/init6487649.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+python3 aiter6487649/test_layernorm2d.py
+python3 aiter6487649/test_rmsnorm2d.py
+python3 aiter6487649/test_gemm_a8w8.py
+python3 aiter6487649/test_moe_2stage.py
+python3 aiter6487649/test_moe_sorting.py
+python3 aiter6487649/test_moe.py
+python3 aiter6487649/test_mha_varlen.py
+python3 aiter6487649/test_mla.py
+python3 aiter6487649/test_gemm_a8w8_blockscale.py
+python3 aiter6487649/test_moe_blockscale.py
+python3 aiter6487649/test_moe_tkw1.py
+python3 aiter6487649/test_pa.py
+python3 aiter6487649/test_moeTopkSoftmax.py
+python3 aiter6487649/test_pa_v1.py
