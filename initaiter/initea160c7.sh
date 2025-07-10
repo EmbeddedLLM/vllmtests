ba81a15f99da288420c8314580874890b13bc5d0
tjtanaa <tunjian.tan@embeddedllm.com>
2025-07-10 05:35:57 +0000

    add init aiter ea160c7 commit
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

---
+++ initaiter/initea160c7.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python3 aiterea160c7/test_gemm_a8w8_blockscale.py
+python3 aiterea160c7/test_gemm_a8w8.py
+python3 aiterea160c7/test_gemm.py
+python3 aiterea160c7/test_kvcache.py
+python3 aiterea160c7/test_layernorm2d.py
+python3 aiterea160c7/test_mha_varlen.py
+python3 aiterea160c7/test_mla.py
+python3 aiterea160c7/test_moe_2stage.py
+python3 aiterea160c7/test_moe_blockscale.py
+python3 aiterea160c7/test_moe_ep.py
+python3 aiterea160c7/test_moe_sorting.py
+python3 aiterea160c7/test_moe_tkw1.py
+python3 aiterea160c7/test_moe.py
+python3 aiterea160c7/test_moeTopkSoftmax.py
+python3 aiterea160c7/test_pa_v1.py
+python3 aiterea160c7/test_pa.py
+python3 aiterea160c7/test_quant.py
+python3 aiterea160c7/test_rmsnorm2d.py
