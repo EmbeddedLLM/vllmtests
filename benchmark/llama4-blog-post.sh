cd4ff86a3a34f5338d10ec2e534dff3b6a9ce975
tjtanaa <tunjian.tan@embeddedllm.com>
2025-08-26 06:46:29 +0000

    log latest benchmark script
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- benchmark/llama4-blog-post.sh
+++ benchmark/llama4-blog-post.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-declare -a in_len=(2000 1000 5000 10000 3200)
-declare -a out_len=(150 1000 1000 1000 800)
+declare -a in_len=(1000)
+declare -a out_len=(1000)
+
 
 help() {
     echo "Usage:"
@@ -12,36 +13,53 @@ help() {
 
 # Define test cases
 declare -a test_cases=(
-    "meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching no aiter"
-    "meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching yes aiter"
-    "meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching no aiter"
-    "meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching yes aiter"
+    # "meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE"
+    # "meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE"
+    # "meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE"
+    # "meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE"
+
+    "meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE default max-batched-tokens"
+    "meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE default max-batched-tokens"
+    "meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE default max-batched-tokens"
+    "meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE default max-batched-tokens"
 )
 
 # Define environment variables for each test case
 declare -A env_vars=(
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching no aiter"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching yes aiter"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=0 VLLM_ROCM_USE_AITER_RMSNORM=0"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching no aiter"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching yes aiter"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=0 VLLM_ROCM_USE_AITER_RMSNORM=0"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE"]="SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=1"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=0"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE"]="SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=1"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=0"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE default max-batched-tokens"]="SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=1"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE default max-batched-tokens"]="SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=1"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE default max-batched-tokens"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=0"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE default max-batched-tokens"]="VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1 SAFETENSORS_FAST_GPU=1 VLLM_USE_V1=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=0"
 
 )
 
 
 # Define model parameters for each test case
 declare -A model_params=(
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching no aiter"]="meta-llama/Llama-4-Maverick-17B-128E-Instruct -tp 8  --max_num_batched_tokens 32768 --max-num-seqs 1024 --max-model-len 36000  --gpu_memory_utilization 0.95 --no-enable-prefix-caching"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching yes aiter"]="meta-llama/Llama-4-Maverick-17B-128E-Instruct -tp 8  --max_num_batched_tokens 32768 --max-num-seqs 1024 --max-model-len 36000  --gpu_memory_utilization 0.95 --no-enable-prefix-caching"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching no aiter"]="meta-llama/Llama-4-Maverick-17B-128E-Instruct -tp 8 --max_num_batched_tokens 32768 --max-num-seqs 1024 --max-model-len 36000  --gpu_memory_utilization 0.95"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching yes aiter"]="meta-llama/Llama-4-Maverick-17B-128E-Instruct -tp 8 --max_num_batched_tokens 32768 --max-num-seqs 1024 --max-model-len 36000  --gpu_memory_utilization 0.95"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max_num_batched_tokens 32768 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}'"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max_num_batched_tokens 32768 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}'"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max_num_batched_tokens 32768 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}' --kv-cache-dtype fp8"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max_num_batched_tokens 32768 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}' --kv-cache-dtype fp8"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE default max-batched-tokens"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}'"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE default max-batched-tokens"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}' --kv-cache-dtype fp8"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE default max-batched-tokens"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}'"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE default max-batched-tokens"]="meta-llama/Llama-3.3-70B-Instruct -tp 1 --max-model-len 32768 --gpu_memory_utilization 0.9 --no-enable-prefix-caching --compilation-config '{\"cudagraph_mode\":\"FULL_AND_PIECEWISE\"}' --kv-cache-dtype fp8"
 
 )
 
 declare -A folder_name=(
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching no aiter"]="Llama-4-Maverick-17B-128E-Instruct-v1-Chunked-Size-32768-no-prefix-caching-no-aiter"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 no prefix caching yes aiter"]="Llama-4-Maverick-17B-128E-Instruct-v1-Chunked-Size-32768-no-prefix-caching-yes-aiter"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching no aiter"]="Llama-4-Maverick-17B-128E-Instruct-v1-Chunked-Size-32768-yes-prefix-caching-no-aiter"
-    ["meta-llama/Llama-4-Maverick-17B-128E-Instruct v1 Chunked Size 32768 yes prefix caching yes aiter"]="Llama-4-Maverick-17B-128E-Instruct-v1-Chunked-Size-32768-yes-prefix-caching-yes-aiter"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE"]="Llama-3.3-70B-Instruct-v1-aiter-mha-FULL_AND_PIECEWISE"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE"]="Llama-3.3-70B-Instruct-v1-aiter-prefill-decode-attn-FULL_AND_PIECEWISE"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE"]="Llama-3.3-70B-Instruct-v1-aiter-mha-fp8-FULL_AND_PIECEWISE"
+    # ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE"]="Llama-3.3-70B-Instruct-v1-aiter-prefill-decode-attn-fp8-FULL_AND_PIECEWISE"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha FULL_AND_PIECEWISE default max-batched-tokens"]="Llama-3.3-70B-Instruct-v1-aiter-mha-FULL_AND_PIECEWISE-default-max-batched-tokens"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter mha fp8 FULL_AND_PIECEWISE default max-batched-tokens"]="Llama-3.3-70B-Instruct-v1-aiter-mha-fp8-FULL_AND_PIECEWISE-default-max-batched-tokens"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn FULL_AND_PIECEWISE default max-batched-tokens"]="Llama-3.3-70B-Instruct-v1-aiter-prefill-decode-attn-FULL_AND_PIECEWISE-default-max-batched-tokens"
+    ["meta-llama/Llama-3.3-70B-Instruct v1 aiter prefill-decode-attn fp8 FULL_AND_PIECEWISE default max-batched-tokens"]="Llama-3.3-70B-Instruct-v1-aiter-prefill-decode-attn-fp8-FULL_AND_PIECEWISE-default-max-batched-tokens"
 
 )
 
@@ -85,10 +103,11 @@ main() {
         esac
     done
 
+
     mkdir -p logs_4
 
     for test_case in "${test_cases[@]}"; do
-        res_dir="llama4_blogpost/prefil_decoder_attn_true/${folder_name[$test_case]}"
+        res_dir="llama3_blogpost/aiter_mha_true/${folder_name[$test_case]}"
 
         echo "Running online serving benchmark for: $test_case"
         log_file="logs_4/$(get_log_filename "$test_case")"
@@ -104,7 +123,7 @@ main() {
 
         # Start vLLM server
         echo "Starting vLLM server..."
-        eval "$full_env vllm serve ${model_params[$test_case]} --trust-remote-code --distributed-executor-backend mp --swap-space 16 --disable-log-requests --compilation-config '{\"full_cuda_graph\": true}' --port 8007 $SUFFIX >> $server_log_file &"
+        eval "$full_env vllm serve ${model_params[$test_case]} --trust-remote-code --distributed-executor-backend mp --swap-space 16 --disable-log-requests --port 8007 $SUFFIX >> $server_log_file &"
         server_pid=$!
         # --no-enable-chunked-prefill
         # Wait for server to be ready
@@ -118,39 +137,19 @@ main() {
         # Extract model name from model_params
         model_name=$(echo "${model_params[$test_case]}" | awk '{print $1}')
 
-        # Loop 1: Dataset-based benchmark
-        # if [ -n "$DATASET" ]; then
-        #     echo "$log_file"
-        #     echo "Running dataset benchmark..." | tee -a "$log_file"
-        #     python3 /path/to//vllm/benchmarks/benchmark_serving.py \
-        #         --backend vllm \
-        #         --model "$model_name" \
-        #         --dataset-name sharegpt \
-        #         --dataset-path "$DATASET" \
-        #         --save-result \
-        #         --max-concurrency 64 \
-        #         --num-prompts 640 \
-        #         --ignore-eos \
-        #         --percentile_metrics ttft,tpot,itl,e2el \
-        #         --result-filename "sharegpt.json" \
-        #         --result-dir "/path/to//$res_dir"\
-        #         --port 8007 >> "$log_file" 2>&1
-        #     sleep 10
-        # fi
-
         # Loop 2: Random prompt benchmarks
         for i in "${!in_len[@]}"; do
             _in_len=${in_len[i]}
             _out_len=${out_len[i]}
             echo "Running random benchmark: in_len=$_in_len, out_len=$_out_len" | tee -a "$log_file"
-            python3 /path/to//vllm/benchmarks/benchmark_serving.py \
+            python3 ../../vllm/benchmarks/benchmark_serving.py \
                 --backend vllm \
                 --model "$model_name" \
                 --dataset-name random \
                 --random-input-len "$_in_len" \
                 --save-result \
                 --result-filename "ISL-$_in_len-OSL-$_out_len.json" \
-                --result-dir "/path/to//$res_dir" \
+                --result-dir "$res_dir" \
                 --port 8007 \
                 --max-concurrency 64 \
                 --num-prompts 640 \
