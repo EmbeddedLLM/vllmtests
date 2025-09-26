c1831710f8e3cdec98f401c9eef9d89c1e01d460
tjtanaa <tunjian.tan@embeddedllm.com>
2025-09-26 03:37:54 +0000

    update the benchmark command to vllm bench serve
    Signed-off-by: tjtanaa <tunjian.tan@embeddedllm.com>

--- visualeval/benchmark_server.sh
+++ visualeval/benchmark_server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python3 ../../fix-specdec-rocm/benchmarks/benchmark_serving.py  \
---backend openai-chat   \
---model Qwen/Qwen2.5-VL-7B-Instruct   \
---endpoint /v1/chat/completions   \
---dataset-name hf   \
---dataset-path lmarena-ai/VisionArena-Chat   \
---hf-split train   \
+vllm bench serve --backend openai-chat \
+--endpoint_type openai-chat \
+--endpoint /v1/chat/completions \
+--model Qwen/Qwen2.5-VL-72B-Instruct \
+--dataset-name hf \
+--dataset-path lmarena-ai/VisionArena-Chat \
+--hf-split train \
 --num-prompts 1000 \
---max-concurrency 64 \
---port 7899 \
-> speedtest_torch_upstream.log 2>&1
+--port 6789 \
+> speedtest_compilesizes_expandedcudagraphsize_largebatchsize.log 2>&1
+# --max-concurrency 64 \
